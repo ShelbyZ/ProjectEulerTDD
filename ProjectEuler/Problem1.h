69623bd1f788b79d501f29b44b1cdb40580da60d
ShelbyZ <user@shagman.codes>
2016-01-12 09:56:05 -0800

    Initial Code Commit

---
+++ ProjectEuler/Problem1.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "Exports.h"
+
+class PROJECTEULER_API Problem1 {
+public:
+	Problem1(void);
+};
---
+++ ProjectEulerTests/Problem1Tests.cpp
@@ -0,0 +1,17 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "..\ProjectEuler\Problem1.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjectEulerTests
+{		
+	TEST_CLASS(Problem1Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
