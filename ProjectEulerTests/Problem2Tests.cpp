82da722016b8778fd4db3e768e16961a3667f612
ShelbyZ <user@shagman.codes>
2016-01-27 14:50:30 -0800

    red-commit - FibonacciSumEven_Input3_Returns2

--- ProjectEulerTests/Problem2Tests.cpp
+++ ProjectEulerTests/Problem2Tests.cpp
@@ -21,5 +21,11 @@ namespace ProjectEulerTests
             auto result = Problem2::FibonacciSumEven(2);
             Assert::AreEqual(2, result);
         }
+
+        TEST_METHOD(FibonacciSumEven_Input3_Returns2)
+        {
+            auto result = Problem2::FibonacciSumEven(3);
+            Assert::AreEqual(2, result);
+        }
     };
 }
